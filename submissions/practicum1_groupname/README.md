93109b55b525905e4bd294d75c92ef306c069a2d
lalopey <lalopey@gmail.com>
2020-09-29 15:36:01 +0400

    (feat) Implemented simple query locally and in minikube

---
+++ submissions/practicum1_groupname/README.md
@@ -0,0 +1,28 @@
+Practicum 1 - Art Search Engine
+==============================
+
+Abnormal Distribution
+
+Local build
+------------
+    
+docker build -t simple_query:frontend -f Docker_simple_query_frontend .
+docker build -t database:db -f Docker_maindb .   
+docker-compose up -d
+docker-compose down
+
+Minikube
+------------
+
+minikube start
+eval $(minikube docker-env)
+docker build -t simple_query:frontend -f Docker_simple_query_frontend .
+docker build -t database:db -f Docker_maindb .   
+
+kubectl apply -f webapp_configmap.yaml
+kubectl apply -f webapp_db_deployment_k8s.yaml
+kubectl apply -f simple_query_deployment_k8s.yaml
+minikube service webapp-simplequery-service
+
+minikube delete
+eval $(minikube -u minikube docker-env)
---
+++ submissions/practicum1_groupname/simplequeryfrontend/simple_query.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import sys
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def mainm():
+    if request.method == 'POST':  # User clicked submit button
+        image_name = request.form['dataidname'] # Get image name submitted by user
+        image_file = requests.post(url=db_url, json={'image_name': image_name}) # request image file from database
+        full_filename = 'http://0.0.0.0:32500' + image_file.content.decode("utf-8")
+        return render_template('image.html', user_image= full_filename) # render html with image
+    else:
+        return render_template('index.html') # first page
+
+
+if __name__ == "__main__":
+    # determine what the URL for the database should be, port is always 8082 for DB
+    if (len(sys.argv) == 2):
+        db_url = 'http://' + sys.argv[1] + ':8082'
+    else:
+        db_url = "http://0.0.0.0:8082/"
+
+    app.run(host='0.0.0.0', port=8081, debug=True)
