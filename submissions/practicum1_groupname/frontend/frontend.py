fc5e3d9cb95725df011cb091fbda6b8cb96743f1
Jessica A Wijaya <jessica@Jessicas-MacBook-Pro-2.local>
2020-09-30 15:25:59 -0700

    enabling user to upload image

--- submissions/practicum1_groupname/frontend/frontend.py
+++ submissions/practicum1_groupname/frontend/frontend.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
 import sys
 import requests
+import numpy as np
+from base64 import b64encode
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def mainm():
+    return render_template('index.html') # first page 
+
+@app.route('/search', methods=['POST', 'GET'])
+def simpleSearch():
     if request.method == 'POST':  # User clicked submit button
         image_name = request.form['content'] # Get image name submitted by user
         image_file = requests.post(url=db_url, json={'image_name': image_name}) # request image file from database
@@ -14,7 +20,18 @@ def mainm():
                                title=image_name,
                                image_url=image_file.content.decode("utf-8")) # render html with image
     else:
-        return render_template('index.html') # first page
+        print("This is the get method.")
+
+@app.route('/upload', methods=['POST', 'GET'])
+def uploadImage():
+    if request.method == 'POST':
+        encoded = b64encode(request.files['file'].read())
+        imgbase64 = encoded.decode('ascii')
+        mime = "image/jpeg"
+        uri = "data:%s;base64,%s" % (mime, imgbase64)
+        return render_template("searchResult.html", image_url=uri, title="Uploaded Image")
+    else:
+        print("This is the get method.")
 
 
 if __name__ == "__main__":
