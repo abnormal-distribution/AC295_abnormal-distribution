c0a912366bb6d1368961daab0f900a4cd99eeecd
Rohit Beri <beri.rohit@gmail.com>
2020-10-01 18:51:49 +0800

    minor update on frontend

--- submissions/practicum1_groupname/frontend/frontend.py
+++ submissions/practicum1_groupname/frontend/frontend.py
@@ -13,7 +13,7 @@ def mainm():
 def simpleSearch():
     if request.method == 'POST':  # User clicked submit button
         image_name = request.form['content'] # Get image name submitted by user
-        image_file = requests.post(url=db_url, json={'image_name': image_name}) # request image file from database
+        image_file = requests.post(url=db_url_1, json={'image_name': image_name}) # request image file from database
         #full_filename = 'http://0.0.0.0:32500' + image_file.content.decode("utf-8")
         return render_template('searchResult.html',
                                title=image_name,
@@ -37,8 +37,10 @@ if __name__ == "__main__":
     # determine what the URL for the database should be, port is always 8082 for DB
 
     if (len(sys.argv) >= 2):
-        db_url = 'http://' + sys.argv[1] + ':8082'
+        db_url_1 = 'http://' + sys.argv[1] + ':8082'
+        db_url_2 = 'http://' + sys.argv[2] + ':8083'
     else:
-        db_url = "http://0.0.0.0:8082/"
+        db_url_1 = "http://0.0.0.0:8082/"
+        db_url_2 = "http://0.0.0.0:8083/"
 
     app.run(host='0.0.0.0', port=8081, debug=True)
