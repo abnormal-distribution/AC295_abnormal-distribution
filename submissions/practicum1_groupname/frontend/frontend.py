b61dcb4168c61a78fcf5195da08033faed4d6aaf
Rohit Beri <beri.rohit@gmail.com>
2020-10-03 18:06:36 +0800

    GCE PV & PVC PATH Updated

--- submissions/practicum1_groupname/frontend/frontend.py
+++ submissions/practicum1_groupname/frontend/frontend.py
@@ -6,11 +6,11 @@ import numpy as np
 import sys
 import requests
 
-""""
+
 PATH = "images-data/gap_images/"
 """
 PATH = 'https://storage.googleapis.com/practicum1-abnormal-distribution/static/gap_images/'
-
+"""
 
 
 app = Flask(__name__)
