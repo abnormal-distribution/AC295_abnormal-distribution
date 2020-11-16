6f7cac7162f8c4c1bd6f327a8a6da1078e7c3800
Jessica A Wijaya <jessica@Jessicas-MacBook-Pro-2.local>
2020-11-16 01:22:58 -0800

    adding model architecture to frontend

--- submissions/practicum2_Abnormal-Distribution/frontend/frontend.py
+++ submissions/practicum2_Abnormal-Distribution/frontend/frontend.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import pandas as pd
-
+import cv2
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
-from transformers import BertTokenizer
-import cv2
+from transformers import BertTokenizer, TFBertModel
+from tensorflow import keras
+from tensorflow.python.keras import backend as K
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras import layers, activations, optimizers, losses, metrics, initializers, regularizers
 
+
+# define global variables
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+IMG_CHANNELS = 3
+num_classes = 10
 answer_list = list(pd.read_csv("static/data/answers.csv", index_col=0).index)
 
 app = Flask(__name__)
@@ -33,6 +42,56 @@ def create_test_pipeline(image, question):
 
     return (image, (question_input, question_type, question_attention))
 
+def build_vqa_model(image_height, image_width, num_channels, num_classes):
+    # Handle to pretrained model (Use a different model here)
+    input_shape=[image_height, image_width, num_channels]
+    resnet = keras.applications.VGG19(
+        include_top=False, 
+        weights='imagenet', 
+        input_shape=input_shape
+    )
+    resnet.trainable = False
+    image_hidden_states = layers.Flatten()(resnet.output)
+    image_hs = layers.Lambda(lambda x: K.l2_normalize(x,axis=1))(image_hidden_states)
+
+    input_ids = layers.Input(shape=(24,), dtype=tf.int32)
+    token_type_ids = layers.Input(shape=(24,), dtype=tf.int32)
+    attention_mask = layers.Input(shape=(24,), dtype=tf.int32)
+
+    bert = TFBertModel.from_pretrained('bert-base-uncased', return_dict=True)
+    bert.trainable = False
+    question = bert(
+        input_ids, 
+        token_type_ids=token_type_ids, 
+        attention_mask=attention_mask
+    )
+    question_hs = layers.Flatten()(question[1])
+    #question_hs = layers.Lambda(lambda x: K.l2_normalize(x,axis=1))(question_hs)
+
+    cross_hs = layers.concatenate([image_hs, question_hs])
+
+    x = layers.Dense(1024, activation='relu')(cross_hs)
+
+    output = layers.Dense(units=num_classes)(x)
+
+    model = Model(inputs=[resnet.input, (input_ids, token_type_ids, attention_mask)], outputs=output)
+
+    return model
+
+def load_model(image_height, image_width, num_channels, num_classes):
+	# build model architecture
+	model = build_vqa_model(image_height, image_width, num_channels, num_classes)
+	learning_rate = 0.001
+	optimizer = optimizers.Adam(lr=learning_rate)
+	loss = losses.SparseCategoricalCrossentropy(from_logits=True)
+	model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+
+	# load model weights
+	# model.load_weights(...)
+
+	return model
+
+model = load_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS, num_classes)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -66,10 +125,10 @@ def index():
 		else: 
 			preprocessed_input = create_test_pipeline(image = cv2.imread(images[idx]), question = selections[idx])
 
-		# print(input) #debug
-		# predicted_answer = model.predict(input)[0] 
-		# answer_index = np.argmax(predicted_answer) 
-		# answers[idx] = answer_list[answer_index]
+		# print(model)
+		predicted_answer = model.predict(preprocessed_input)[0] 
+		answer_index = np.argmax(predicted_answer) 
+		answers[idx] = answer_list[answer_index]
 
 		return render_template("index.html", 
 			images = images, 
