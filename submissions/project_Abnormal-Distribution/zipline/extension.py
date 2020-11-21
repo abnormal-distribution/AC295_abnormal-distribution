7384b6c21762d12abd07b5a2675d9acb30854771
lalopey <lalopey@gmail.com>
2020-11-21 20:11:07 +0400

    (feat) Added zipline ingest and starter code in zipline.ipynb notebook

--- submissions/project_Abnormal-Distribution/zipline/extension.py
+++ submissions/project_Abnormal-Distribution/zipline/extension.py
@@ -2,11 +2,6 @@ import pandas as pd
 
 from zipline.data.bundles import register
 from zipline.data.bundles.csvdir import csvdir_equities
-
-
-
-
-import pandas as pd
 from zipline.utils.calendars import get_calendar
 import sys
 
